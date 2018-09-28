3627e7bd9602712466dbd81f3e4801270b671d67
15257 <15257@mcl>
2018-09-28 17:40:23 +0800

    mapreduce、spark with java、spark with scala版wordcount 环境测试ok

--- hadoop-study/src/main/java/mapreduce/job/wordcount/WordCountRunner.java
+++ hadoop-study/src/main/java/mapreduce/job/wordcount/WordCountRunner.java
@@ -1,8 +1,48 @@
 package mapreduce.job.wordcount;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCountRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		Configuration conf = new Configuration();
+		
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(WordCountRunner.class);
+		job.setJobName("wordcount_mapreduce");
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		args = new String[2];
+		args[0] = "D:/BONC/wordcount.txt";
+		args[1] = "D:/BONC/wordcount_out";
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setCompressOutput(job, false); 
+		
+        Path path = new Path(args[1]);
+        FileSystem fileSystem = path.getFileSystem(conf);
+        if (fileSystem.exists(path)) {
+			fileSystem.delete(path, true);
+			System.out.println(args[1]+"已存在，删除");
+		}
+		
+		if(job.waitForCompletion(true)){
+			System.out.println("success");
+		}
 	}
 }
